8acce021beafa29933c210a6b0dce6b28c4d027c
Ankit Singh <76651002+ankitnarwariya@users.noreply.github.com>
2023-10-18 20:19:44 +0530

    Update and rename test.js to 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = '001';
+let accountEmail = 'ankit@gmail.com';
+var accountPass = "12345";
+accountCity = "Gwalior";
+
+//accountId = '002';
+accountEmail = 'ankitSingh@gmail.com';
+accountPass = "123456789";
+accountCity = "Indore";
+
+//console.log(accountCity);
+console.table([accountId, accountEmail, accountPass, accountCity])
